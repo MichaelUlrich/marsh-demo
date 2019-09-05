b4c278670b4d6b64a76b0a3d7d285726c5c90178
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2019-09-05 12:21:33 -0400

    Add files via upload

---
+++ precache-manifest.fab94af00874e65628499099c66ac147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a6155b77c8f20dd509f1b1e86aa67b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93c75eba4d16de435f02",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "5dc9cbba5fed6d55b8ae",
+    "url": "/static/css/main.a12da2d5.chunk.css"
+  },
+  {
+    "revision": "93c75eba4d16de435f02",
+    "url": "/static/js/2.68d43c96.chunk.js"
+  },
+  {
+    "revision": "5dc9cbba5fed6d55b8ae",
+    "url": "/static/js/main.52282aa4.chunk.js"
+  },
+  {
+    "revision": "31fa81a77b7de97fc844",
+    "url": "/static/js/runtime~main.ccc6dfe0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/marsh-demo/precache-manifest.6d30a28cd019c445af0e690efa180e2a.js"
+  "/precache-manifest.fab94af00874e65628499099c66ac147.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marsh-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
