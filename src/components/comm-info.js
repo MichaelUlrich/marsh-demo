d9d8e4f10947a59c8da5f1c94e67c490b183d659
Michael Ulrich <MichaelUlrich@users.noreply.github.com>
2019-09-06 10:55:38 -0400

    Re-named about.js to comm-info.js

--- src/components/comm-info.js
+++ src/components/comm-info.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { Grid, Cell} from 'react-mdl';
-class About extends Component {
+class Commision extends Component {
+  /*
+    Replace with new contact image url
+    Add contact info - email, twitter, FA
+  */
   render() {
     return(
       <div className="about-body">   {/* Double curly braces for inline styling */}
         <Grid className="about-grid">
-          <Cell col={6}>
-            <img src="https://cdn.discordapp.com/attachments/429905389954400266/611275382699720793/legout.png"
-              style={{position: 'absolute', padding: '50px'}}></img>
-          </Cell>
-          <Cell col={6}>
+          <Cell col={12}>
             <div className="about-text">
-              <h1>My name here</h1>
+              <h1>Commision Info Here</h1>
               <hr/>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -20,9 +20,13 @@ class About extends Component {
               </p>
             </div>
           </Cell>
+          <Cell col={12}>
+            <img className="landing-img" src="https://cdn.discordapp.com/attachments/429905389954400266/611392527525871629/cardychexample-no-text.png"
+              style={{height: '350px', margin:'auto', padding: '50px'}}></img>
+          </Cell>
         </Grid>
       </div>
     )
   }
 }
-export default About;
+export default Commision;
